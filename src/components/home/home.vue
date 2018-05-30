eb0f4f29a23124c821bf7a29b78511fad7e9572b
虞伟凯 <weikai.yu@suanzi.cn>
2018-05-30 14:13:52 +0800

    update

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -18,48 +18,54 @@
         <p class="introduce-text"> 学生网络信息化服务中心是负责“杭师大微学工”微信公众号、新生网、毕业生离校系统等网络平台的运营与管理，为同学提供综合网络服务的学生组织。中心本着贴近生活，服务学生的宗旨，
           以“杭师大微学工”微信公众号为主要平台，发布师大校园学生工作前沿动态，为师大学生在第一时间内提供各种校园信息，并以互联网为抓手定期开展各类线上线下系列特色活动。</p>
         <div class="btn-section">
-          <el-button type="primary" round>了解更多</el-button>
-          <el-button type="success" round @click="clickLogin">马上报名</el-button>
+          <el-button type="primary" round class="btn" @click="clickKonwUs">了解更多</el-button>
+          <el-button type="success" round @click="clickLogin" class="btn">马上报名</el-button>
         </div>
       </div>
       <div class="help-section">
         <el-row :gutter="100">
           <el-col :lg="6" :md="12" :sm="1">
             <el-card :body-style="{ padding: '0px' }" class="card-section" shadow="hover">
-              <img src="./card-img.jpg" class="help-img"/>
-              <div class="help-title">《入学手册》</div>
+              <img src="./img01.jpg" class="help-img"/>
+              <div class="help-title">《杭师大生活宝典》</div>
             </el-card>
           </el-col>
           <el-col :lg="6" :md="12" :sm="24">
             <el-card :body-style="{ padding: '0px',height:'100%',display:'flex',flexDirection:'column' }"
                      class="card-section" shadow="hover">
-              <img src="./card-img.jpg" class="help-img"/>
-              <div class="help-title">《入学手册》</div>
+              <img src="./img02.jpg" class="help-img"/>
+              <div class="help-title">《杭师大入学指南》</div>
             </el-card>
           </el-col>
           <el-col :lg="6" :md="12" :sm="24">
             <el-card :body-style="{ padding: '0px',height:'100%',display:'flex',flexDirection:'column' }"
                      class="card-section" shadow="hover">
-              <img src="./card-img.jpg" class="help-img"/>
-              <div class="help-title">《入学手册》</div>
+              <img src="./img03.jpg" class="help-img"/>
+              <div class="help-title">《杭师大交通指南》</div>
             </el-card>
           </el-col>
           <el-col :lg="6" :md="12" :sm="24">
             <el-card :body-style="{ padding: '0px',height:'100%',display:'flex',flexDirection:'column' }"
                      class="card-section" shadow="hover">
-              <img src="./card-img.jpg" class="help-img"/>
-              <div class="help-title">《入学手册》</div>
+              <img src="./img04.jpg" class="help-img"/>
+              <div class="help-title">《开学物品清单》</div>
             </el-card>
           </el-col>
         </el-row>
+      </div>
+      <div class="red-envelope-section">
+
+        <div class="btn-section">
+          <el-button type="primary" round @click="openAlert()" class="btn">活动详情</el-button>
+          <el-tooltip class="item" effect="dark" :content="password" placement="top">
+            <el-button type="success" round class="btn">查看口令</el-button>
+          </el-tooltip>
+        </div>
       </div>
       <div class="activity-section">
         <a href="http://www.189.cn/" target="_blank" style="cursor:default;">
           <img src="./dianxin01.jpg" class="dianxin-img"/>
         </a>
-        <el-tooltip class="item" effect="dark" :content="password" placement="top">
-          <el-button type="warning" round class="password-btn">查看口令</el-button>
-        </el-tooltip>
       </div>
       <div class="end-section" ref="endSection">
         <transition enter-active-class="animated fade fadeInRight">
@@ -68,7 +74,7 @@
             <h2 class="end-sm-text">我们在杭州师范大学等你</h2>
           </div>
         </transition>
-        <el-button type="warning" round class="join-btn" @click="clickLogin">加入我们</el-button>
+        <el-button type="warning" round class="join-btn btn" @click="clickLogin">加入我们</el-button>
       </div>
     </div>
   </div>
@@ -79,7 +85,7 @@
   export default {
     data () {
       return {
-        password: '这是测试信息',
+        password: '手机套餐我就选电信',
         headerAnimshow: false,
         endAnimShow:false,
         navScrolled:false
@@ -133,9 +139,20 @@
 
 
     },
+    computed:{
+
+    },
     methods: {
       clickLogin(){
         this.$router.push('/login')
+      },
+      openAlert(){
+        this.$alert('信息君将在6月20日、6月21日、6月22日,22:00派发千元现金红包口令，届时请点击查看口令页面，查看支付宝红包口令。','活动详情',{
+          confirmButtonText: '确定'
+        })
+      },
+      clickKonwUs(){
+          this.$router.push("/introduce")
       }
     },
     components:{
@@ -149,6 +166,9 @@
   .container
     width 100%
     height 100%
+    .btn
+      font-size 0.8rem
+      padding 0.5rem 1rem
     .home-main
       width 100%
       .background-section
@@ -169,11 +189,11 @@
           align-items center;
           color white
           .title
-            font-size 100px
+            font-size 6rem
             line-height 100px
             margin-bottom 20px
           .smill-title
-            font-size 20px
+            font-size 2rem
       .introduce-section
         position relative
         display flex
@@ -184,17 +204,17 @@
         min-height 500px
         margin-top 50px
         .more-text
-          font-size 20px
+          font-size 1rem
           color #666
         .introduce-title-text
           color black
           margin-top 20px
           font-weight bold
-          font-size 50px
+          font-size 2rem
         .introduce-text
           margin-top 20px
           max-width 60%
-          font-size 25px
+          font-size 1.5rem
         .btn-section
           display flex
           flex-direction row
@@ -216,17 +236,23 @@
             width 100%
           .help-title
             padding 14px
-            font-size 15px
+            font-size 1rem
+      .red-envelope-section
+        position relative
+        width 100%
+        redbag-bg-height('./hongbao')
+        .btn-section
+          position absolute
+          bottom 5%
+          right 5%
+
       .activity-section
         position relative
         width 100%
         cursor default
         .dianxin-img
           width 100%
-        .password-btn
-          position absolute
-          bottom 5%
-          right 5%
+
       .end-section
         display flex
         flex-direction column
@@ -248,11 +274,11 @@
             text-align center
             color: white
             font-weight bold
-            font-size 50px
+            font-size 3rem
           .end-sm-text
             color white
             margin-top 30px
-            font-size 25px
+            font-size 1rem
         .join-btn
           margin-top 30px
 
---
+++ src/components/introduce/introduce.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="container">
+    <myNav :scrolled="false" :fixed="false" :btnToIndex="true"></myNav>
+    <div class="header-section">
+      <h1 class="header-title">信息君</h1>
+      <div class="campus-section">
+        <span class="text-yellow">仓前</span>
+        <span class="text-blue">校区</span>
+      </div>
+      <h5 class="header-small-text">仅仅只是开始</h5>
+      <h5 class="header-small-text">Just Getting Started</h5>
+    </div>
+    <div class="main-section">
+      <div class="title-section">
+        <span>Meet&nbsp;&nbsp;</span>
+        <span>The&nbsp;&nbsp;</span>
+        <span>Team</span>
+      </div>
+      <div class="introduce-section">
+        <p>陌生的城市，陌生的校园与期待你到来的我们。</p>
+        <p>学生网络信息化服务中心，是由一群无私奉献、乐于助人的小哥哥小姐姐组成的团队，致力于为全校师生提供便利服务。是不是听不懂？那就请往下拉~</p>
+
+      </div>
+      <div class="member-section">
+        <div class="row-section">
+          <div class="member-item-section" @click="centerDialogVisible = true">
+            <img class="member-header" src="./ic_head.jpg"/>
+            <div class="member-name">
+              部长 某某某
+            </div>
+            <div class="member-college">
+              阿里巴巴商学院
+            </div>
+            <div class="member-class">
+              电商132
+            </div>
+          </div>
+          <div class="member-item-section">
+            <img class="member-header" src="./ic_head.jpg"/>
+            <div class="member-name">
+              部长 某某某
+            </div>
+            <div class="member-college">
+              阿里巴巴商学院
+            </div>
+            <div class="member-class">
+              电商132
+            </div>
+          </div>
+          <div class="member-item-section">
+            <img class="member-header" src="./ic_head.jpg"/>
+            <div class="member-name">
+              部长 某某某
+            </div>
+            <div class="member-college">
+              阿里巴巴商学院
+            </div>
+            <div class="member-class">
+              电商132
+            </div>
+          </div>
+          <div class="member-item-section">
+            <img class="member-header" src="./ic_head.jpg"/>
+            <div class="member-name">
+              部长 某某某
+            </div>
+            <div class="member-college">
+              阿里巴巴商学院
+            </div>
+            <div class="member-class">
+              电商132
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <el-dialog
+      title="组织部"
+      :visible.sync="centerDialogVisible"
+      width="30%"
+      center>
+      <div class="dialog-content-section">
+        <img class="dialog-member-header" src="./ic_head.jpg"/>
+        <div class="dialog-member-name">某某某</div>
+        <span class="dialog-department-content">负责组织内部事务<br>
+      （1）负责中心干部、干事绩效考核<br>
+      （2）负责中心干事招新、破冰大会等活动<br>
+      （3）负责中心内部通知的发送与人力资源调配<br>
+      （5）负责安排办公室值班表及值班考勤<br>
+      （6）负责中心统一采购及管理中心的物资<br>
+      （7）负责中心经费的使用情况记录及报<br></span>
+        <el-button type="primary" @click="centerDialogVisible = false" class="dialog-login-button">立即报名</el-button>
+      </div>
+
+    </el-dialog>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import myNav from '../nav/nav.vue'
+  export default {
+    data(){
+      return{
+        centerDialogVisible: false
+      }
+
+    },
+    created(){
+
+    },
+    components:{
+      myNav
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    .container
+      width 100%
+      height 100%
+      box-sizing border-box
+      .header-section
+        display flex
+        justify-content center
+        flex-direction column
+        align-items center
+        .header-title
+          color #f16651
+          font-size 96px
+          font-weight bold
+          line-height 100px
+          margin-top 80px
+          margin-bottom 50px
+        .campus-section
+          font-size 96px
+          font-weight bold
+          margin-bottom 30px
+          .text-yellow
+            color #fab117
+          .text-blue
+            color #5fc8d7
+        .header-small-text
+          color #9b9b9b
+          font-size 25px
+      .main-section
+        display flex
+        padding 2rem
+        flex-direction column
+        align-items center
+        .title-section
+          display flex
+          flex-direction row
+          justify-content center
+          font-size 3rem
+          font-weight 3rem
+          margin-top 3rem
+          margin-bottom 3rem
+        .introduce-section
+          display flex
+          flex-direction column
+          align-items center
+          width 60%
+          color #9b9b9b
+          font-size 1.5rem
+        .member-section
+          width 80%
+          padding 10px
+          margin-top 2rem
+          .row-section
+            display flex
+            flex-direction row
+            justify-content center
+            .member-item-section
+              width 20%
+              display flex
+              flex-direction column
+              align-items center
+              justify-content center
+              .member-header
+                width 80%
+                height 80%
+                border-radius 50%
+              .member-name
+                margin-top 20px
+                color #9b9b9b
+                font-size 24px
+              .member-college
+                margin-top 15px
+                color #00546e
+              .member-class
+                margin-top 10px
+                color #5fc8d7
+    .dialog-content-section
+      display flex
+      flex-direction column
+      justify-content center
+      align-items center
+      .dialog-member-header
+        width 60%
+        height 60%
+        border-radius 50%
+      .dialog-member-name
+        font-size 25px
+        margin-top 20px
+      .dialog-department-content
+        align-self flex-start
+      .dialog-login-button
+        align-self flex-end
+
+</style>
